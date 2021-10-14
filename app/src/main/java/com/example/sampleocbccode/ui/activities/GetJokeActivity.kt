fa7dbb2e0e49839b422efc0bcf76b5ede81c2a3f
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-14 17:55:00 +0800

    Replace setContentView with databinding

--- app/src/main/java/com/example/sampleocbccode/ui/activities/GetJokeActivity.kt
+++ app/src/main/java/com/example/sampleocbccode/ui/activities/GetJokeActivity.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sampleocbccode.ui.adapters.JokeAdapter
 import com.example.sampleocbccode.R
+import com.example.sampleocbccode.databinding.ActivityGetjokeBinding
 import com.example.sampleocbccode.domain.feature.joke.model.Joke
 import com.example.sampleocbccode.domain.feature.joke.repository.JokeRepository
 import com.example.sampleocbccode.domain.network.RetrofitService
@@ -20,22 +22,19 @@ class GetJokeActivity : AppCompatActivity() {
 
     private var listOfJokes: MutableList<Joke> = mutableListOf()
     private val jokeRepository = JokeRepository()
+    private lateinit var databinding: ActivityGetjokeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_getjoke)
-
-        //Init UI elements
-        val getJokeButton = findViewById<Button>(R.id.getJokeButton)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        databinding = DataBindingUtil.setContentView(this, R.layout.activity_getjoke)
 
         //Initialize RecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        databinding.recyclerView.layoutManager = LinearLayoutManager(this)
         val adapter = JokeAdapter(listOfJokes)
-        recyclerView.adapter = adapter
+        databinding.recyclerView.adapter = adapter
 
         //OnClickListener
-        getJokeButton.setOnClickListener {
+        databinding.getJokeButton.setOnClickListener {
             CoroutineScope(Dispatchers.IO).launch {
                 val jokes = jokeRepository.getJoke()
                 withContext(Dispatchers.Main) {
