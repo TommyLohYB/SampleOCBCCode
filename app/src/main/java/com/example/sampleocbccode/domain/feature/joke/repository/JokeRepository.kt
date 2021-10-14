de27e1c9d24d138ff6fa6ebfafcc9c2adfb3d156
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-14 14:00:32 +0800

    Create Joke Repo

--- app/src/main/java/com/example/sampleocbccode/domain/feature/joke/repository/JokeRepository.kt
+++ app/src/main/java/com/example/sampleocbccode/domain/feature/joke/repository/JokeRepository.kt
@@ -3,26 +3,15 @@ package com.example.sampleocbccode.domain.feature.joke.repository
 import android.util.Log
 import com.example.sampleocbccode.domain.feature.joke.model.Joke
 import com.example.sampleocbccode.domain.network.RetrofitService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class JokeRepository {
 
-     fun getJoke(): List<Joke> {
-        var jokes: List<Joke> = listOf()
-        GlobalScope.launch(Dispatchers.IO) {
-            jokes = getJokeFromApi()
-        }
-        return jokes
-    }
-
-    suspend fun getJokeFromApi () : List<Joke>{
-        var jokes: List<Joke> = listOf()
+    suspend fun getJoke(): MutableList<Joke> {
+        var jokes: MutableList<Joke> = mutableListOf()
         val getJokeResponse = RetrofitService.jokeApi.getJokes()
         if (getJokeResponse.isSuccessful) {
-            jokes = getJokeResponse.body()?.jokes ?: listOf()
+            jokes = (getJokeResponse.body()?.jokes ?: mutableListOf<Joke>()) as MutableList<Joke>
         }
         return jokes
     }
--- app/src/main/java/com/example/sampleocbccode/ui/activities/GetJokeActivity.kt
+++ app/src/main/java/com/example/sampleocbccode/ui/activities/GetJokeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sampleocbccode.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,16 +12,13 @@ import com.example.sampleocbccode.R
 import com.example.sampleocbccode.domain.feature.joke.model.Joke
 import com.example.sampleocbccode.domain.feature.joke.repository.JokeRepository
 import com.example.sampleocbccode.domain.network.RetrofitService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
 class GetJokeActivity : AppCompatActivity() {
 
-    private var listOfJokes: List<Joke> = listOf()
+    private var listOfJokes: MutableList<Joke> = mutableListOf()
     private val jokeRepository = JokeRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +27,7 @@ class GetJokeActivity : AppCompatActivity() {
 
         //Init UI elements
         val getJokeButton = findViewById<Button>(R.id.getJokeButton)
-        val recyclerView  = findViewById<RecyclerView>(R.id.recyclerView)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //Initialize RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -38,13 +36,18 @@ class GetJokeActivity : AppCompatActivity() {
 
         //OnClickListener
         getJokeButton.setOnClickListener {
-            val jokes = jokeRepository.getJoke()
-            if(jokes.isNotEmpty()){
-                listOfJokes = jokes
-                adapter.notifyDataSetChanged()
-            }else{
-                val toastMessage  = "Unable to get jokes"
-                Toast.makeText(applicationContext,toastMessage, Toast.LENGTH_SHORT).show()
+            CoroutineScope(Dispatchers.IO).launch {
+                val jokes = jokeRepository.getJoke()
+                withContext(Dispatchers.Main) {
+                    if (jokes.isNotEmpty()) {
+                        listOfJokes.clear()
+                        listOfJokes.addAll(jokes)
+                        adapter.notifyDataSetChanged()
+                    } else {
+                        val toastMessage = "Unable to get jokes"
+                        Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
     }
