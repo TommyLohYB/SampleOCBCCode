71747192a184b7148b0694e65c59b5a780b8b519
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-14 17:41:12 +0800

    Update SubmitJokeActivity.kt
    1. Remove findViewById
    2. Use data binding instead

--- app/src/main/java/com/example/sampleocbccode/ui/activities/SubmitJokeActivity.kt
+++ app/src/main/java/com/example/sampleocbccode/ui/activities/SubmitJokeActivity.kt
@@ -1,15 +1,19 @@
 package com.example.sampleocbccode.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.sampleocbccode.R
+import com.example.sampleocbccode.databinding.ActivitySubmitjokeBinding
 import com.example.sampleocbccode.domain.feature.joke.service.JokesAPI
 import com.example.sampleocbccode.domain.feature.joke.model.JokeFlag
 import com.example.sampleocbccode.domain.feature.joke.model.SubmitJokeRequest
 import com.example.sampleocbccode.domain.feature.joke.repository.JokeRepository
+import com.example.sampleocbccode.domain.feature.joke.viewmodel.NewJokeViewModel
 import com.example.sampleocbccode.domain.network.RetrofitService
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
@@ -20,18 +24,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 class SubmitJokeActivity : AppCompatActivity() {
 
     private val jokeRepository = JokeRepository()
+    private lateinit var viewModel: NewJokeViewModel
+    private lateinit var databinding: ActivitySubmitjokeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_submitjoke)
-
-        val submitJokeEditText = findViewById<EditText>(R.id.submitJokeEditText)
-        val submitJokeButton = findViewById<Button>(R.id.submitJokeButton)
+        databinding = DataBindingUtil.setContentView(this,R.layout.activity_submitjoke)
         var toastMessage: String = ""
-        var failedSubmissionMessage = "Failed to submit joke. Please try again!"
 
-        submitJokeButton.setOnClickListener {
-            if (submitJokeEditText.text.isNullOrBlank()) {
+        databinding.submitJokeButton.setOnClickListener {
+            if (databinding.submitJokeEditText.text.isNullOrBlank()) {
                 toastMessage = "Please do not leave it blank"
                 Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_SHORT).show()
             } else {
@@ -40,7 +42,7 @@ class SubmitJokeActivity : AppCompatActivity() {
                         3,
                         "Misc",
                         "single",
-                        submitJokeEditText.text.toString(),
+                        databinding.submitJokeEditText.text.toString(),
                         JokeFlag(
                             nsfw = false,
                             religious = false,
