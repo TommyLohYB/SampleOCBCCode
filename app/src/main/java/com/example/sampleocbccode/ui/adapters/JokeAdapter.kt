564c1da8e11a8bb1807ff36deecc198029f756ab
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-14 18:15:53 +0800

    Update RecyclerView to integrate data binding

--- app/src/main/java/com/example/sampleocbccode/ui/adapters/JokeAdapter.kt
+++ app/src/main/java/com/example/sampleocbccode/ui/adapters/JokeAdapter.kt
@@ -4,31 +4,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sampleocbccode.R
+import com.example.sampleocbccode.databinding.ItemJokeBinding
 import com.example.sampleocbccode.domain.feature.joke.model.Joke
 
-class JokeAdapter(private var jokes: MutableList<Joke>) : RecyclerView.Adapter<JokeAdapter.ViewHolder>(){
+class JokeAdapter(private var jokes: MutableList<Joke>) :
+    RecyclerView.Adapter<JokeAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_joke, parent, false)
-        return ViewHolder(view)
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ItemJokeBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.item_joke, parent, false
+        )
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val joke = jokes[position]
-        holder.jokeSetupTextView.text = joke.setup
-        holder.jokeDeliveryTextView.text = joke.delivery
+        holder.bind(jokes[position])
     }
 
     override fun getItemCount(): Int {
         return jokes.size
     }
 
-    // Holds the views for adding it to image and text
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val jokeSetupTextView: TextView = itemView.findViewById(R.id.jokeSetupTextView)
-        val jokeDeliveryTextView: TextView = itemView.findViewById(R.id.jokeDeliveryTextView)
+    class ViewHolder(val binding: ItemJokeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Joke) {
+            binding.apply {
+                jokeSetup = item.setup
+                jokeDelivery = item.delivery
+            }
+        }
     }
 }
