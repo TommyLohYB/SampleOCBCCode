27ec72fe97ccd17b757064f0e6b847ae7479b20c
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-14 17:42:44 +0800

    Remove initial creation of ViewModel

--- app/src/main/java/com/example/sampleocbccode/ui/activities/SubmitJokeActivity.kt
+++ app/src/main/java/com/example/sampleocbccode/ui/activities/SubmitJokeActivity.kt
@@ -1,30 +1,19 @@
 package com.example.sampleocbccode.ui.activities
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.sampleocbccode.R
 import com.example.sampleocbccode.databinding.ActivitySubmitjokeBinding
-import com.example.sampleocbccode.domain.feature.joke.service.JokesAPI
 import com.example.sampleocbccode.domain.feature.joke.model.JokeFlag
 import com.example.sampleocbccode.domain.feature.joke.model.SubmitJokeRequest
 import com.example.sampleocbccode.domain.feature.joke.repository.JokeRepository
-import com.example.sampleocbccode.domain.feature.joke.viewmodel.NewJokeViewModel
-import com.example.sampleocbccode.domain.network.RetrofitService
 import kotlinx.coroutines.*
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class SubmitJokeActivity : AppCompatActivity() {
 
     private val jokeRepository = JokeRepository()
-    private lateinit var viewModel: NewJokeViewModel
     private lateinit var databinding: ActivitySubmitjokeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
