ed73f75c382e54812f8ea59d1163efbe5f644e6b
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-18 14:19:49 +0800

    Creation of SampleApplication to include @HiltAndroidApp annotation

---
+++ app/src/main/java/com/example/sampleocbccode/ui/application/SampleApplication.kt
@@ -0,0 +1,8 @@
+package com.example.sampleocbccode.ui.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SampleApplication : Application(){
+}
