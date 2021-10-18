5be27092cbb4cf10e18515475658d9085ad02422
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-18 10:30:00 +0800

    Create ApplicationModule.kt

---
+++ app/src/main/java/com/example/sampleocbccode/domain/ApplicationModule.kt
@@ -0,0 +1,35 @@
+package com.example.sampleocbccode.domain
+import com.example.sampleocbccode.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationModule::class)
+class ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient ():OkHttpClient{
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL:String): Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+
+}
