0c31fb04f59e37695941449d48bae6c0f310e7cb
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-18 16:01:29 +0800

    Included jokesApi dependency

--- app/src/main/java/com/example/sampleocbccode/domain/feature/joke/repository/JokeRepository.kt
+++ app/src/main/java/com/example/sampleocbccode/domain/feature/joke/repository/JokeRepository.kt
@@ -2,14 +2,18 @@ package com.example.sampleocbccode.domain.feature.joke.repository
 
 import com.example.sampleocbccode.domain.feature.joke.model.Joke
 import com.example.sampleocbccode.domain.feature.joke.model.SubmitJokeRequest
+import com.example.sampleocbccode.domain.feature.joke.service.JokesAPI
 import com.example.sampleocbccode.domain.network.RetrofitService
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
-class JokeRepository @Inject constructor(){
+class JokeRepository @Inject constructor(private val jokesAPI: JokesAPI) {
 
     suspend fun getJoke(): MutableList<Joke> {
         var jokes: MutableList<Joke> = mutableListOf()
-        val getJokeResponse = RetrofitService.jokeApi.getJokes()
+        val getJokeResponse = jokesAPI.getJokes()
         if (getJokeResponse.isSuccessful) {
             jokes = (getJokeResponse.body()?.jokes ?: mutableListOf<Joke>()) as MutableList<Joke>
         }
@@ -17,7 +21,7 @@ class JokeRepository @Inject constructor(){
     }
 
     suspend fun submitJoke(submitJokeRequest: SubmitJokeRequest): String {
-        val submitJokeResponse = RetrofitService.jokeApi.submitJoke(submitJokeRequest)
+        val submitJokeResponse = jokesAPI.submitJoke(submitJokeRequest)
         if (submitJokeResponse.isSuccessful) {
             return submitJokeResponse.body()?.message ?: "error"
         }
