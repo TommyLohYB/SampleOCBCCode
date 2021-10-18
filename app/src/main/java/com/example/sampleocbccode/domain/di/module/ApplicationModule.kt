c164e9340d454b063f7f1f4c80391f6a62d01c69
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-18 16:01:40 +0800

    Update ApplicationModule.kt

--- app/src/main/java/com/example/sampleocbccode/domain/di/module/ApplicationModule.kt
+++ app/src/main/java/com/example/sampleocbccode/domain/di/module/ApplicationModule.kt
@@ -1,33 +1,44 @@
 package com.example.sampleocbccode.domain.di.module
+import android.provider.SyncStateContract
+import com.example.sampleocbccode.domain.feature.joke.service.JokesAPI
+import com.example.sampleocbccode.domain.network.RetrofitService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationModule::class)
-class ApplicationModule {
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
 
-    @Provides
     @Singleton
+    @Provides
     fun provideOkHttpClient ():OkHttpClient{
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         return OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
     }
 
-    @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient, BASE_URL:String): Retrofit{
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit{
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
-            .baseUrl(BASE_URL)
+            .baseUrl("https://v2.jokeapi.dev/")
             .build()
+    }
+
+    @Provides
+    fun provideApiClient (retrofit: Retrofit): JokesAPI{
+        return retrofit.create(JokesAPI::class.java)
     }
 
 
