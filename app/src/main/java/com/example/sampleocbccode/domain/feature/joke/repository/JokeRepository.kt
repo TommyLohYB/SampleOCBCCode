c7a16fb1fdfa81dbe462167d7eceee39471ffbd9
Tommy <yubin.loh.2016@smu.edu.sg>
2021-10-18 14:35:36 +0800

    Update JokeRepository.kt

--- app/src/main/java/com/example/sampleocbccode/domain/feature/joke/repository/JokeRepository.kt
+++ app/src/main/java/com/example/sampleocbccode/domain/feature/joke/repository/JokeRepository.kt
@@ -1,12 +1,11 @@
 package com.example.sampleocbccode.domain.feature.joke.repository
 
-import android.util.Log
 import com.example.sampleocbccode.domain.feature.joke.model.Joke
 import com.example.sampleocbccode.domain.feature.joke.model.SubmitJokeRequest
 import com.example.sampleocbccode.domain.network.RetrofitService
-import kotlinx.coroutines.*
+import javax.inject.Inject
 
-class JokeRepository {
+class JokeRepository @Inject constructor(){
 
     suspend fun getJoke(): MutableList<Joke> {
         var jokes: MutableList<Joke> = mutableListOf()
